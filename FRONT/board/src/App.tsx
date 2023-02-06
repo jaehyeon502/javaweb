b4b48b5a83d6b060cc79c48814801e493d22e0eb
jaehyeon <hyesung6516@naver.com>
2023-02-06 16:54:57 +0900

    modified

---
+++ FRONT/board/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginView from './view/LoginView';
+import NavigationBar from './view/NavigationBar';
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <LoginView />
+    </>
+  );
+}
+
+export default App;
