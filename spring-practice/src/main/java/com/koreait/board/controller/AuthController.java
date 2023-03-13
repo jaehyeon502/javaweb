70125048307ec6025110f586df56055b8162d871
jaehyeon <hyesung6516@naver.com>
2023-03-13 15:19:04 +0900

    230313 15:19

--- spring-practice/src/main/java/com/koreait/board/controller/AuthController.java
+++ spring-practice/src/main/java/com/koreait/board/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.koreait.board.common.constant.ApiMappingPattern;
 import com.koreait.board.provider.TokenProvider;
+import com.koreait.board.service.AuthService;
 
 @RestController
 @RequestMapping(ApiMappingPattern.AUTH)
@@ -15,8 +16,12 @@ public class AuthController {
     
     @Autowired private TokenProvider tokenProvider;
 
+    @Autowired private AuthService authService;
+
     private static final String GET_TOKEN = "/getToken";
     private static final String VALIDATE_TOKEN = "/validateToken";
+    private static final String BCRYPT = "/bcrypt";
+    private static final String DECRYPT = "/decrypt";
 
     @PostMapping(GET_TOKEN)
     public String getToken() {
@@ -26,6 +31,16 @@ public class AuthController {
     @PostMapping(VALIDATE_TOKEN)
     public String validateToken(@RequestBody String requestbody) {
         return tokenProvider.validate(requestbody);
+    }
+
+    @PostMapping(BCRYPT)
+    public String bcrypt(@RequestBody String requestBody) {
+        return authService.bcrypt(requestBody);
+    }
+
+    @PostMapping(DECRYPT)
+    public boolean decrypt(@RequestBody String requestBody) {
+        return authService.decrypt(requestBody);
     }
 
 }
