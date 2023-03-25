ead17f09376aef787b389cf2df5c92fb4b9c4499
jaehyeon <hyesung6516@naver.com>
2023-03-25 16:18:24 +0900

    230325 16:18

---
+++ FRONT/board/src/interface/Liky.interface.ts
@@ -0,0 +1,6 @@
+interface Liky{
+    boardNumber: number;
+    userProfileUrl?: string | null;
+    userNickname: string;
+}
+export default Liky;
---
+++ FRONT/board/src/interface/User.interface.ts
@@ -0,0 +1,8 @@
+interface User{
+    email: string;
+    nickname: string;
+    telNumber: string;
+    address: string;
+    profile?: string | null;
+}
+export default User;
